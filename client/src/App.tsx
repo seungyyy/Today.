2f79875f3e168ba967096f4cc00f9b4ea2da4dd3
seungyeon.lee <dus3214@gmail.com>
2022-11-17 22:44:06 +0900

    add: router 연결

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DiaryCreate from './pages/DiaryCreate';
+import DiaryEdit from './pages/DiaryEdit';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/:id/create" element={<DiaryCreate />}></Route>
+        <Route path="/:id/edit" element={<DiaryEdit />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/DiaryCreate.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DiaryCreate = () => {
+  return (
+    <div>DiaryCreate</div>
+  )
+}
+
+export default DiaryCreate
