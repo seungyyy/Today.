1b521c6b7081a9078cf78d3d2c6e727556186fcf
seungyeon.lee <dus3214@gmail.com>
2022-11-22 00:03:32 +0900

    feat: 회원가입페이지 구현

--- client/src/components/common/InputBox.tsx
+++ client/src/components/common/InputBox.tsx
@@ -3,16 +3,30 @@ import styled from 'styled-components';
 
 type ContainerProps = {
   children: React.ReactNode;
+  title: string;
+  onClick?: ()=> void;
 }
 
-const InputBox = ({children}: ContainerProps) => {
+const InputBox = ({children, title, onClick}: ContainerProps) => {
   return (
-    <Container>
+    <Container onClick={onClick}>
+      <h2 className='blind' >마침 {title}</h2>
       {children}
     </Container>
   )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  .blind {
+    position: absolute;
+    overflow: hidden;
+    clip: rect(0 0 0 0);
+    margin: -1px;
+    width: 1px;
+    height: 1px;
+  }
+`;
 
 export default InputBox
--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,5 +1,7 @@
-import e from 'express';
-import React, {useState, useEffect, useRef} from 'react'
+import React, {useState, useRef} from 'react';
+import { useNavigate } from 'react-router-dom';
+import InputBox from '../components/common/InputBox';
+
 
 interface SignInProps {
   email: string;
@@ -7,6 +9,7 @@ interface SignInProps {
 }
 
 const SignIn = () => {
+  const navigation = useNavigate()
   const formRef = useRef<HTMLFormElement>(null);
   const [signInData, setSignInData] = useState<SignInProps>({
     email: '',
@@ -26,15 +29,20 @@ const SignIn = () => {
   }
 
   return (
-    <>
+    <InputBox title='로그인'>
     <form ref={formRef} onSubmit={handlerSubmit}>
       <label htmlFor='email'>이메일</label>
       <input type={'text'} name="email" value={signInData.email} id="email" onChange={handlerInputOnChange}/>
       <label htmlFor='password'>비밀번호</label>
       <input type={'password'} name="password" value={signInData.password} onChange={handlerInputOnChange}/>
       <button type='submit'>로그인</button>
+      <button type='button'>카카오 로그인</button>
     </form>
-    </>
+    <div>
+      <button type='button' onClick={()=> navigation('/signup')}>회원가입</button>
+      <button type='button'>비밀번호 찾기</button>
+    </div>
+    </InputBox>
   )
 }
 
